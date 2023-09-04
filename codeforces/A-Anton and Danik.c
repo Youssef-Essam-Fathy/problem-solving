aecc9eea44b09f68ec9c3edb3928f57b29dc6717
Youssef Essam <113689276+Youssef-Essam-Fathy@users.noreply.github.com>
2023-09-04 14:25:27 +0300

    A-Anton and Danik problem solved

---
+++ codeforces/A-Anton and Danik.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int A = 0, D = 0, n,i;
+	scanf("%d", &n); // int -> 6
+	char arr[n];
+	scanf("%s", arr); // string -> A | D
+
+	for (i = 0; i < n; i++)
+		arr[i] == 'A' ? A++ : D++;
+
+	//A < D | A = D | A > D
+	printf((A > D) ? "Anton" : ((A == D) ? "Friendship" : "Danik"));
+
+	return 0;
+}
